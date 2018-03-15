fd4106bb953eb8c0b5f22b7efc7e45fd1a1f810f
Nathan Huberty <nathan.huberty@orange.fr>
2018-03-15 19:36:11 +0100

    feature over, need more adjustments later

--- source/javascripts/text-converter.js
+++ source/javascripts/text-converter.js
@@ -1,12 +1,12 @@
 
-const lyrics = document.querySelectorAll(".lyrics");
+const lyrics = document.querySelectorAll("p");
 const emojis = document.querySelectorAll(".emojis");
 const textConverter = document.querySelector("#text-converter");
 
-toggleOpacity() {
+function toggleOpacity() {
   emojis.forEach(line => line.classList.toggle('hidden'));
   lyrics.forEach(line => line.classList.toggle('visible'));
   textConverter.classList.toggle('active');
-})
+}
 
-textConverter.addEventListener('click', toggleOpacity());
+textConverter.addEventListener('click', () => toggleOpacity() );
